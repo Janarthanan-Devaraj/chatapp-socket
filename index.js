6056c06c053c64f09cdf799b59951d3d4838e96a
jana-sanjay <djanarthanan614@gmail.com>
2023-04-04 10:49:22 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let express = require("express");
-let cors = require("cors");
 let http = require("http");
+let cors = require("cors");
 let bodyParser = require("body-parser");
+const { Server } = require("socket.io")
 let path = require("path");
 
 const port = 9000
 
 app = express();
 
-const server = http.createServer(app).listen(port, () => {});
+const server = http.createServer(app);
 
 app.use(cors())
 
@@ -16,16 +17,26 @@ app.use(express.static(path.join(__dirname,"client")));
 
 app.use(bodyParser.json())
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  }
+})
+
 app.post("/server", (req, res) => {
-    io.emit("command", req.body);
-    console.log(req.body);
-    res.status(201).json({status: "reached" });
+  io.emit("command", req.body);
+  console.log(req.body);
+  res.status(201).json({status: "reached" });
 });
 
-let io = require("socket.io").listen(server);
 
 io.on("connection", (socket) => {
     socket.on("command",function (data) {
       io.emit("command", data);
+      console.log(data)
     });
+});
+
+server.listen(port, () => {
+  console.log("SERVER RUNNING")
 });
